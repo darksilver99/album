61a37ee03fe30767ea7b749d19e3e592e85d768e
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-03-06 02:43:04 +0000

    Updating to latest FlutterFlow output.

---
+++ lib/components/remove_background_view_model.dart
@@ -0,0 +1,34 @@
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'remove_background_view_widget.dart' show RemoveBackgroundViewWidget;
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class RemoveBackgroundViewModel
+    extends FlutterFlowModel<RemoveBackgroundViewWidget> {
+  ///  Local state fields for this component.
+
+  bool isRemoveBackground = false;
+
+  String? returnPhotoPath;
+
+  ///  State fields for stateful widgets in this component.
+
+  // State field(s) for Checkbox widget.
+  bool? checkboxValue;
+
+  /// Initialization and disposal methods.
+
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+
+  /// Action blocks are added here.
+
+  /// Additional helper methods are added here.
+}
---
+++ lib/components/remove_background_view_widget.dart
@@ -0,0 +1,191 @@
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'remove_background_view_model.dart';
+export 'remove_background_view_model.dart';
+
+class RemoveBackgroundViewWidget extends StatefulWidget {
+  const RemoveBackgroundViewWidget({
+    super.key,
+    required this.photoPath,
+  });
+
+  final String? photoPath;
+
+  @override
+  State<RemoveBackgroundViewWidget> createState() =>
+      _RemoveBackgroundViewWidgetState();
+}
+
+class _RemoveBackgroundViewWidgetState
+    extends State<RemoveBackgroundViewWidget> {
+  late RemoveBackgroundViewModel _model;
+
+  @override
+  void setState(VoidCallback callback) {
+    super.setState(callback);
+    _model.onUpdate();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _model = createModel(context, () => RemoveBackgroundViewModel());
+
+    // On component load action.
+    SchedulerBinding.instance.addPostFrameCallback((_) async {
+      _model.returnPhotoPath = widget.photoPath;
+    });
+  }
+
+  @override
+  void dispose() {
+    _model.maybeDispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    context.watch<FFAppState>();
+
+    return Align(
+      alignment: AlignmentDirectional(0.0, 1.0),
+      child: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          color: FlutterFlowTheme.of(context).secondaryBackground,
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(0.0),
+            bottomRight: Radius.circular(0.0),
+            topLeft: Radius.circular(16.0),
+            topRight: Radius.circular(16.0),
+          ),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Padding(
+              padding: EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 8.0),
+              child: Stack(
+                children: [
+                  Align(
+                    alignment: AlignmentDirectional(1.0, -1.0),
+                    child: Padding(
+                      padding:
+                          EdgeInsetsDirectional.fromSTEB(0.0, 8.0, 8.0, 0.0),
+                      child: InkWell(
+                        splashColor: Colors.transparent,
+                        focusColor: Colors.transparent,
+                        hoverColor: Colors.transparent,
+                        highlightColor: Colors.transparent,
+                        onTap: () async {
+                          Navigator.pop(context);
+                        },
+                        child: Icon(
+                          Icons.close_rounded,
+                          color: FlutterFlowTheme.of(context).secondaryText,
+                          size: 32.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            if (widget.photoPath != null && widget.photoPath != '')
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(16.0, 0.0, 16.0, 16.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(0.0),
+                  child: Image.network(
+                    _model.returnPhotoPath!,
+                    width: double.infinity,
+                    fit: BoxFit.contain,
+                  ),
+                ),
+              ),
+            Padding(
+              padding: EdgeInsetsDirectional.fromSTEB(16.0, 0.0, 16.0, 16.0),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Theme(
+                    data: ThemeData(
+                      checkboxTheme: CheckboxThemeData(
+                        visualDensity: VisualDensity.compact,
+                        materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(4.0),
+                        ),
+                      ),
+                      unselectedWidgetColor:
+                          FlutterFlowTheme.of(context).secondaryText,
+                    ),
+                    child: Checkbox(
+                      value: _model.checkboxValue ??= false,
+                      onChanged: (newValue) async {
+                        setState(() => _model.checkboxValue = newValue!);
+                        if (newValue!) {
+                          setState(() {
+                            _model.isRemoveBackground = true;
+                          });
+                        } else {
+                          setState(() {
+                            _model.isRemoveBackground = false;
+                          });
+                        }
+                      },
+                      activeColor: FlutterFlowTheme.of(context).primary,
+                      checkColor: FlutterFlowTheme.of(context).info,
+                    ),
+                  ),
+                  Text(
+                    'Remove background',
+                    style: FlutterFlowTheme.of(context).bodyMedium.override(
+                          fontFamily: 'Inter',
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsetsDirectional.fromSTEB(16.0, 0.0, 16.0, 0.0),
+              child: FFButtonWidget(
+                onPressed: () async {
+                  Navigator.pop(context, _model.returnPhotoPath);
+                },
+                text: 'Continue',
+                options: FFButtonOptions(
+                  width: double.infinity,
+                  height: 40.0,
+                  padding: EdgeInsetsDirectional.fromSTEB(24.0, 0.0, 24.0, 0.0),
+                  iconPadding:
+                      EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
+                  color: FlutterFlowTheme.of(context).primary,
+                  textStyle: FlutterFlowTheme.of(context).titleSmall.override(
+                        fontFamily: 'Inter',
+                        color: Colors.white,
+                      ),
+                  elevation: 3.0,
+                  borderSide: BorderSide(
+                    color: Colors.transparent,
+                    width: 1.0,
+                  ),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
