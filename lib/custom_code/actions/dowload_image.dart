10cf80dbf9fffe0fe3f33e6fdb2864e0e09cfdc1
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-03-06 04:21:56 +0000

    Updating to latest FlutterFlow output.

--- lib/custom_code/actions/dowload_image.dart
+++ lib/custom_code/actions/dowload_image.dart
@@ -8,6 +8,12 @@ import 'package:flutter/material.dart';
 // Begin custom action code
 // DO NOT REMOVE OR MODIFY THE CODE ABOVE!
 
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'dart:io';
+
 Future dowloadImage(String? photoPath) async {
   // Add your function code here!
+  final file = File(photoPath!);
+  Uint8List bytes = await file.readAsBytes();
+  await ImageGallerySaver.saveImage(bytes);
 }
